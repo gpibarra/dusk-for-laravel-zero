b169c70d833c0c72eb0d416eedb5c2315473ef78
Gerardo Ibarra <gibarra@upgradeit.com.ar>
2021-01-11 19:55:00 -0300

    first commit

---
+++ src/DuskForLaravelZeroServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NunoMaduro\DuskForLaravelZero;
+
+use Laravel\Dusk\DuskServiceProvider as ServiceProvider;
+
+class DuskForLaravelZeroServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+}
